898b68b8f6880a3b98a7f67d9d00d9652ff63a2d
karmusha <karmusha@gmail.com>
2022-08-24 14:25:10 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+
+var read = Console.ReadLine();
+var number = Convert.ToInt32(read); // Конвертация в число.
+
+int result = number * number;
+
+Console.WriteLine(result);
+
