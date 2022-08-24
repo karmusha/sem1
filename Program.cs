fb0e14e66fd64ccf46690760295d33833fa8f448
karmusha <karmusha@gmail.com>
2022-08-24 15:27:19 +0200

    1 и 2 задачи с семинара 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число");
+﻿// // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
-var read = Console.ReadLine();
-var number = Convert.ToInt32(read); // Конвертация в число.
+// Console.WriteLine("Напишите первое число");
 
-int result = number * number;
+// var read = Console.ReadLine();
+// var a = Convert.ToInt32(read);
 
-Console.WriteLine(result);
+// Console.WriteLine("Напишите второе число");
+
+// read = Console.ReadLine();
+// var b = Convert.ToInt32(read);
+// var sqrt = b * b;
+
+// bool is_sqrt = a == sqrt;
+
+// Console.WriteLine(is_sqrt);
+
+// if (is_sqrt == true) 
+// {
+//     Console.WriteLine("Число а является квадратным корнем числа b.");
+// } 
+// else 
+// {
+//     Console.WriteLine("Число а не является квадратным корнем числа b.");
+// }
+
+// // Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+// var week_day = "";
+
+// Console.WriteLine("Введите число дня недели.");
+// var read = Console.ReadLine(); // Если раннее была объявлена переменна read, то удалить "var" в этой строке.
+// var day_number = Convert.ToInt32(read);
+
+// if (day_number == 1) 
+// {
+// week_day = "понедельник";
+// } 
+
+// if (day_number == 2) 
+// {
+// week_day = "вторник";
+// }
+
+// if (day_number == 3) 
+// {
+// week_day = "среда";
+// }
+
+// if (day_number == 4) 
+// {
+// week_day = "четверг";
+// }
+
+// if (day_number == 5) 
+// {
+// week_day = "пятница";
+// }
+
+// if (day_number == 6) 
+// {
+// week_day = "суббота";
+// }
+
+// if (day_number == 7) 
+// {
+// week_day = "воскресенье";
+// }
+
+// Console.WriteLine(week_day);
 
