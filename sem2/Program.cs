a3a3fe6ecac5739fca03cfd7797a6b373f054008
karmusha <karmusha@gmail.com>
2022-08-27 17:17:37 +0200

    added sem2

---
+++ sem2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Random random = new Random(); // генерит рандомное число
+int randomNumber = random.Next(10, 100); // ...в диапазоне [10, 99]
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 10;
+int lastNumber = randomNumber % 10;
+
+Console.WriteLine(firstNumber);
+Console.WriteLine(lastNumber);
+
+if (firstNumber > lastNumber)
+{
+    Console.WriteLine($"Максимальное число равно {firstNumber}"); // Интерполяция. Способ вывести в одну строку.
+}
+else
+{
+    Console.WriteLine("Максимальное число равно " + lastNumber);
+}
+
