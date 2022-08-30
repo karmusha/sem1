62743ce85796a1002f2055da05bc74826db72525
karmusha <karmusha@gmail.com>
2022-08-30 22:21:42 +0200

    sem3 task 18 and 21

--- sem3/Program.cs
+++ sem3/Program.cs
@@ -41,41 +41,123 @@
 
 // Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
+// {
+//     Console.WriteLine("Введите X:");
+//     bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
+
+//     Console.WriteLine("Введите Y:");
+//     bool isNumberY = int.TryParse(Console.ReadLine(), out int y);
+
+//     if (isNumberX != true || isNumberY != true)
+//     {
+//         Console.WriteLine("Числа введены не верно.");
+//     }
+
+//     bool Validate(int x, int y)
+//     {
+//         if (x == 0 || y == 0) return false;
+//         return true;
+//     }
+
+//     int GetQuater(int x, int y)
+//     {
+//         if (x > 0 && y > 0) return 1;
+//         if (x < 0 && y > 0) return 2;
+//         if (x < 0 && y < 0) return 3;
+//         if (x > 0 && y < 0) return 4;
+
+//         throw new Exception("Числа не верны.");
+//     }
+
+//     bool isValid = Validate(x, y);
+//     if (isValid == false)
+//     {
+//         Console.WriteLine("Какое-то число равно нулю.");
+//         return;
+//     }
+
+//     int result = GetQuater(x, y);
+//     Console.WriteLine(result);
+// }
+
+// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// {
+//     Console.WriteLine("Введите номер четверти области координат:");
+//     bool isNumberQ = int.TryParse(Console.ReadLine(), out int q);
+
+//     bool IsNum (int q)
+//     {
+//         if(q >=1 && q <= 4)
+//         {
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     string Coordinates(int q)
+//     {
+//         string coord = "";
+//         if (q == 1)
+//         {
+//             coord = "x > 0, y > 0";
+//         }
+//         if (q == 2)
+//         {
+//             coord = "x < 0, y > 0";
+//         }
+//         if (q == 3)
+//         {
+//             coord = "x < 0, y < 0";
+//         }
+//         if (q == 4)
+//         {
+//             coord = "x > 0, y < 0";
+//         }
+//         return coord;
+//     }
+
+//     if (!IsNum(q))
+//     {
+//         Console.WriteLine("Вы ввели неверный номер области координат. Введите номер от 1 до 4.");
+//     }
+
+//     string result = Coordinates(q);
+//     Console.WriteLine(result);
+// }
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3, 6); B (2, 1) -> 5,09
+// A (7, -5); B (1, -1) -> 7,21
+
 {
-    Console.WriteLine("Введите X:");
-    bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
+    Console.WriteLine("Введите координату X точки A:");
+    bool isNumberXA = int.TryParse(Console.ReadLine(), out int xa);
 
-    Console.WriteLine("Введите Y:");
-    bool isNumberY = int.TryParse(Console.ReadLine(), out int y);
+    Console.WriteLine("Введите координату Y точки A:");
+    bool isNumberYA = int.TryParse(Console.ReadLine(), out int ya);
 
-    if (isNumberX != true || isNumberY != true)
+    Console.WriteLine("Введите координату X точки B:");
+    bool isNumberXB = int.TryParse(Console.ReadLine(), out int xb);
+
+    Console.WriteLine("Введите координату Y точки B:");
+    bool isNumberYB = int.TryParse(Console.ReadLine(), out int yb);
+
+    if (isNumberXA != true || isNumberYA != true || isNumberXB != true || isNumberYB != true)
     {
         Console.WriteLine("Числа введены не верно.");
-    }
-
-    bool Validate(int x, int y)
-    {
-        if (x == 0 || y == 0) return false;
-        return true;
-    }
-
-    int GetQuater(int x, int y)
-    {
-        if (x > 0 && y > 0) return 1;
-        if (x < 0 && y > 0) return 2;
-        if (x < 0 && y < 0) return 3;
-        if (x > 0 && y < 0) return 4;
-
-        throw new Exception("Числа не верны.");
-    }
-
-    bool isValid = Validate(x, y);
-    if (isValid == false)
-    {
-        Console.WriteLine("Какое-то число равно нулю.");
         return;
     }
 
-    int result = GetQuater(x, y);
+    double CalcDistance(int xa, int ya, int xb, int yb)
+    {
+        int x = xb - xa;
+        int y = yb - ya;
+        double sum = Math.Pow(x,2) + Math.Pow(y,2);
+        double distance = Math.Sqrt(sum);
+        return distance;
+    }
+
+    double result = CalcDistance(xa, ya, xb, yb);
     Console.WriteLine(result);
 }
