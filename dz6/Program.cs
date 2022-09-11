f2cf7b80e95bdb2b3c738a9c947b6705c136589b
karmusha <karmusha@gmail.com>
2022-09-11 20:26:32 +0200

    dz6 finished

--- dz6/Program.cs
+++ dz6/Program.cs
@@ -1,2 +1,105 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Task41() // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+{
+    Console.WriteLine("Сколько чисел вы хотите ввести?");
+    bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+
+    if (!isNumber || length <= 0)
+    {
+        Console.WriteLine("Попробуйте ввести другую длинну.");
+        return;
+    }
+
+    int[] array = new int[length]; 
+
+    bool FillArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++){
+            Console.WriteLine("Введите элемент массива");
+            bool isNumberR = int.TryParse(Console.ReadLine(), out int r);
+            if (!isNumberR){
+                return false;
+            }
+            array[i] = r;
+        }
+        return true;
+    }
+        
+    if (!FillArray(array)){
+        Console.WriteLine("Вы ввели неверный элемент массива");
+        return;
+    }
+
+    void PrintArray(int[] array)
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(i < array.Length - 1)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            else
+            {
+                Console.Write($"{array[i]}");
+            }
+        }
+        Console.Write("]");
+    }
+
+    PrintArray(array);
+    Console.WriteLine();
+
+    int GetPositiveNumbers(int[] array)
+    {
+        int counter = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0) 
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    int result = GetPositiveNumbers(array);
+    Console.WriteLine($"Количество положительных чисел в заданном массиве = {result}.");
+}
+Task41();
+
+void Task43() // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+{
+    Console.WriteLine("У Вас есть две прямых, заданных уравнениями: 1) y = k1 * x + b1, 2) y = k2 * x + b2.");
+    Console.WriteLine("Введите значение k1:");
+    bool isNumberK1 = int.TryParse(Console.ReadLine(), out int k1);
+
+    Console.WriteLine("Введите значение b1:");
+    bool isNumberB1 = int.TryParse(Console.ReadLine(), out int b1);
+
+    Console.WriteLine("Введите значение k2:");
+    bool isNumberK2 = int.TryParse(Console.ReadLine(), out int k2);
+
+    Console.WriteLine("Введите значение b2:");
+    bool isNumberB2 = int.TryParse(Console.ReadLine(), out int b2);
+
+    if (!isNumberK1 || !isNumberB1 || !isNumberK2 || !isNumberB2)
+    {
+        Console.WriteLine("Числа введены не верно.");
+        return;
+    }
+
+    (double, double) GetXY(int b2, int b1, int k1, int k2)
+    {
+        double x = (b2 - b1) / (double)(k1 - k2);
+        double y = k1 * x + b1;
+        return (x, y);
+    }
+
+    (double x, double y) = GetXY(b2, b1, k1, k2);
+    Console.WriteLine($"({x}; {y})");
+}
+Task43();
--- sem3/Program.cs
+++ sem3/Program.cs
@@ -1,162 +1,162 @@
-﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// {string input = Console.ReadLine();
+{string input = Console.ReadLine();
 
-// bool ValidateInput(string input)
-// {
-//     bool isNumber = int.TryParse(input, out int number);
+bool ValidateInput(string input)
+{
+    bool isNumber = int.TryParse(input, out int number);
 
-//     if (!isNumber)
-//     {
-//         return false;
-//     }
+    if (!isNumber)
+    {
+        return false;
+    }
 
-//     if (number < 100 && number > -100)
-//     {
-//         return false;
-//     }
-//     return true;
-// }
+    if (number < 100 && number > -100)
+    {
+        return false;
+    }
+    return true;
+}
 
-// char GetThirdNumber(string number)
-// {
-//     if (number.Contains("-"))
-//     {
-//         return number[3];
-//     }
-//     return number[2];
-// }
+char GetThirdNumber(string number)
+{
+    if (number.Contains("-"))
+    {
+        return number[3];
+    }
+    return number[2];
+}
 
-// bool isValid = ValidateInput(input);
+bool isValid = ValidateInput(input);
 
-// if (!isValid)
-// {
-//     Console.WriteLine("No valid number");
-//     return;
-// }
+if (!isValid)
+{
+    Console.WriteLine("No valid number");
+    return;
+}
 
-// char result = GetThirdNumber(input);
+char result = GetThirdNumber(input);
 
-// Console.WriteLine(result);}
+Console.WriteLine(result);}
 
 // Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-// {
-//     Console.WriteLine("Введите X:");
-//     bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
+{
+    Console.WriteLine("Введите X:");
+    bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
 
-//     Console.WriteLine("Введите Y:");
-//     bool isNumberY = int.TryParse(Console.ReadLine(), out int y);
+    Console.WriteLine("Введите Y:");
+    bool isNumberY = int.TryParse(Console.ReadLine(), out int y);
 
-//     if (isNumberX != true || isNumberY != true)
-//     {
-//         Console.WriteLine("Числа введены не верно.");
-//     }
+    if (isNumberX != true || isNumberY != true)
+    {
+        Console.WriteLine("Числа введены не верно.");
+    }
 
-//     bool Validate(int x, int y)
-//     {
-//         if (x == 0 || y == 0) return false;
-//         return true;
-//     }
+    bool Validate(int x, int y)
+    {
+        if (x == 0 || y == 0) return false;
+        return true;
+    }
 
-//     int GetQuater(int x, int y)
-//     {
-//         if (x > 0 && y > 0) return 1;
-//         if (x < 0 && y > 0) return 2;
-//         if (x < 0 && y < 0) return 3;
-//         if (x > 0 && y < 0) return 4;
+    int GetQuater(int x, int y)
+    {
+        if (x > 0 && y > 0) return 1;
+        if (x < 0 && y > 0) return 2;
+        if (x < 0 && y < 0) return 3;
+        if (x > 0 && y < 0) return 4;
 
-//         throw new Exception("Числа не верны.");
-//     }
+        throw new Exception("Числа не верны.");
+    }
 
-//     bool isValid = Validate(x, y);
-//     if (isValid == false)
-//     {
-//         Console.WriteLine("Какое-то число равно нулю.");
-//         return;
-//     }
+    bool isValid = Validate(x, y);
+    if (isValid == false)
+    {
+        Console.WriteLine("Какое-то число равно нулю.");
+        return;
+    }
 
-//     int result = GetQuater(x, y);
-//     Console.WriteLine(result);
-// }
+    int result = GetQuater(x, y);
+    Console.WriteLine(result);
+}
 
 // Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y).
 
-// {
-//     Console.WriteLine("Введите номер четверти области координат:");
-//     bool isNumberQ = int.TryParse(Console.ReadLine(), out int q);
+{
+    Console.WriteLine("Введите номер четверти области координат:");
+    bool isNumberQ = int.TryParse(Console.ReadLine(), out int q);
 
-//     bool IsNum (int q)
-//     {
-//         if(q >=1 && q <= 4)
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
+    bool IsNum (int q)
+    {
+        if(q >=1 && q <= 4)
+        {
+            return true;
+        }
+        return false;
+    }
 
-//     string Coordinates(int q)
-//     {
-//         string coord = "";
-//         if (q == 1)
-//         {
-//             coord = "x > 0, y > 0";
-//         }
-//         if (q == 2)
-//         {
-//             coord = "x < 0, y > 0";
-//         }
-//         if (q == 3)
-//         {
-//             coord = "x < 0, y < 0";
-//         }
-//         if (q == 4)
-//         {
-//             coord = "x > 0, y < 0";
-//         }
-//         return coord;
-//     }
+    string Coordinates(int q)
+    {
+        string coord = "";
+        if (q == 1)
+        {
+            coord = "x > 0, y > 0";
+        }
+        if (q == 2)
+        {
+            coord = "x < 0, y > 0";
+        }
+        if (q == 3)
+        {
+            coord = "x < 0, y < 0";
+        }
+        if (q == 4)
+        {
+            coord = "x > 0, y < 0";
+        }
+        return coord;
+    }
 
-//     if (!IsNum(q))
-//     {
-//         Console.WriteLine("Вы ввели неверный номер области координат. Введите номер от 1 до 4.");
-//     }
+    if (!IsNum(q))
+    {
+        Console.WriteLine("Вы ввели неверный номер области координат. Введите номер от 1 до 4.");
+    }
 
-//     string result = Coordinates(q);
-//     Console.WriteLine(result);
-// }
+    string result = Coordinates(q);
+    Console.WriteLine(result);
+}
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 // A (3, 6); B (2, 1) -> 5,09
 // A (7, -5); B (1, -1) -> 7,21
 
-// {
-//     Console.WriteLine("Введите координату X точки A:");
-//     bool isNumberXA = int.TryParse(Console.ReadLine(), out int xa);
+{
+    Console.WriteLine("Введите координату X точки A:");
+    bool isNumberXA = int.TryParse(Console.ReadLine(), out int xa);
 
-//     Console.WriteLine("Введите координату Y точки A:");
-//     bool isNumberYA = int.TryParse(Console.ReadLine(), out int ya);
+    Console.WriteLine("Введите координату Y точки A:");
+    bool isNumberYA = int.TryParse(Console.ReadLine(), out int ya);
 
-//     Console.WriteLine("Введите координату X точки B:");
-//     bool isNumberXB = int.TryParse(Console.ReadLine(), out int xb);
+    Console.WriteLine("Введите координату X точки B:");
+    bool isNumberXB = int.TryParse(Console.ReadLine(), out int xb);
 
-//     Console.WriteLine("Введите координату Y точки B:");
-//     bool isNumberYB = int.TryParse(Console.ReadLine(), out int yb);
+    Console.WriteLine("Введите координату Y точки B:");
+    bool isNumberYB = int.TryParse(Console.ReadLine(), out int yb);
 
-//     if (!isNumberXA || !isNumberYA || isNumberXB != true || isNumberYB != true)
-//     {
-//         Console.WriteLine("Числа введены не верно.");
-//         return;
-//     }
+    if (!isNumberXA || !isNumberYA || isNumberXB != true || isNumberYB != true)
+    {
+        Console.WriteLine("Числа введены не верно.");
+        return;
+    }
 
-//     double CalcDistance(int xa, int ya, int xb, int yb)
-//     {
-//         return Math.Sqrt(Math.Pow((xb - xa),2) + Math.Pow((yb - ya),2));
-//     }
+    double CalcDistance(int xa, int ya, int xb, int yb)
+    {
+        return Math.Sqrt(Math.Pow((xb - xa),2) + Math.Pow((yb - ya),2));
+    }
 
-//     double result = CalcDistance(xa, ya, xb, yb);
-//     Console.WriteLine(result);
-// }
+    double result = CalcDistance(xa, ya, xb, yb);
+    Console.WriteLine(result);
+}
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
